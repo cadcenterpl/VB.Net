da2560bbb39dce4d125dd02ee8ac9d0d7814d999
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-10-18 13:53:25 +0200

    v1.0

---
+++ ISI_KM_PO_2_8/ISI_KM_PO_2_8/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x As Boolean = True : Dim y, z As Integer : y = 1 : z = 1
+        Console.WriteLine(działanie.wynik(x, y, z))
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ ISI_KM_PO_2_8/ISI_KM_PO_2_8/działanie.vb
@@ -0,0 +1,8 @@
+﻿Public Class działanie
+    Public Shared Function wynik(x, y, z)
+        z += 1
+        x = (y = 1 Or z = 1)
+        Console.WriteLine("y=" + Str(y) + ", z=" + Str(z))
+        Return x
+    End Function
+End Class
