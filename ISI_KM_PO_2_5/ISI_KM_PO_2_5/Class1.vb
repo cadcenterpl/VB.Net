66e1a710beec790c696cef9be4e302a82c261002
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-10-18 12:50:38 +0200

    v1.0

---
+++ ISI_KM_PO_2_5/ISI_KM_PO_2_5/Class1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Class1
+    Public Shared Function fun1(x, y)
+        x *= y * 2 : Return x
+    End Function
+End Class
---
+++ ISI_KM_PO_2_5/ISI_KM_PO_2_5/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x, y As Integer : x = 2 : y = 3
+        Console.WriteLine(Class1.fun1(x, y))
+        Console.ReadKey()
+    End Sub
+
+End Module
