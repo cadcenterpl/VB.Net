5184f12d5d9a4084666eb3aa07308618948b52aa
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-10-18 12:40:01 +0200

    v1.0

---
+++ ISI_KM_PO_2_4/ISI_KM_PO_2_4/Class1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Class1
+    Public Shared Function liczba(x)
+        Return (++x * 2)
+    End Function
+End Class
---
+++ ISI_KM_PO_2_4/ISI_KM_PO_2_4/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x As Integer = 100
+        Console.WriteLine(Class1.liczba(x))
+        Console.ReadKey()
+    End Sub
+
+End Module
