b4195c1168cb266f65fe5e3c290447c11a525336
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-10-18 13:25:55 +0200

    v1.1

--- ISI_KM_PO_2_6/ISI_KM_PO_2_6/Class1.vb
+++ ISI_KM_PO_2_6/ISI_KM_PO_2_6/Class1.vb
@@ -1,8 +1,8 @@
 ﻿Public Class działanie
     Public Shared Function wynik(x, y) As Double
         y -= 2 : x = y
-        x = ++y + 1
-        x = --y
+        x = ++y - 1
+        x = --y + 1
         Return x
     End Function
 End Class
