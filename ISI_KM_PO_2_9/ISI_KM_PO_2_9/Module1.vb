c8f0022fa4cf34789ef2396cf4adb70ca619e5af
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-10-18 14:49:00 +0200

    v1.0

---
+++ ISI_KM_PO_2_9/ISI_KM_PO_2_9/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Call dane.A()
+        Call dane.B()
+        Call dane.C()
+        Call dane.D()
+        Call dane.E()
+        Call dane.F()
+        Console.ReadKey()
+    End Sub
+End Module
---
+++ ISI_KM_PO_2_9/ISI_KM_PO_2_9/działania.vb
@@ -0,0 +1,32 @@
+﻿Public Class działania
+    Public Shared Function A(w, x, y, z)
+        w = ((++x + 1) > 1 Or y = 4 Or z > 0)
+        Console.Write("x=" + Str(x) + ", y=" + Str(y) + ", z=" + Str(z) + ", ")
+        Return w
+    End Function
+    Public Shared Function B(w, x, y, z)
+        w = (x > 1 And y = 4 Or z > 0)
+        Console.Write("x=" + Str(x) + ", y=" + Str(y) + ", z=" + Str(z) + ", ")
+        Return w
+    End Function
+    Public Shared Function C(w, x, y, z)
+        w = (x > 1 And y = 4 And z > 0)
+        Console.Write("x=" + Str(x) + ", y=" + Str(y) + ", z=" + Str(z) + ", ")
+        Return w
+    End Function
+    Public Shared Function D(w, x, y, z)
+        w = (++x = 1 Or (y + 1) > 2 Or ++z > 0)
+        Console.Write("x=" + Str(x) + ", y=" + Str(y) + ", z=" + Str(z) + ", ")
+        Return w
+    End Function
+    Public Shared Function E(w, x, y, z)
+        w = (++x = 1 And (y + 1) > 2 Or ++z > 0)
+        Console.Write("x=" + Str(x) + ", y=" + Str(y) + ", z=" + Str(z) + ", ")
+        Return w
+    End Function
+    Public Shared Function F(w, x, y, z)
+        w = (++x = 1 And (y + 1) > 2 And ++z > 0)
+        Console.Write("x=" + Str(x) + ", y=" + Str(y) + ", z=" + Str(z) + ", ")
+        Return w
+    End Function
+End Class
