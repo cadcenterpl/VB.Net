c02722db99ae8e63f339dcbf77b3b0b3e79cbcba
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-11-13 13:33:56 +0100

    3.1

---
+++ ISI_KM_PO_3_01/App.vb
@@ -0,0 +1,24 @@
+﻿Public Class App
+    Public Shared Sub start()
+        Console.Write("Podaj rok: ")
+        Dim rok As Int32 = Console.ReadLine()
+        Call Program.action(rok)
+    End Sub
+
+    Shared Function check(rok)
+        If ((rok Mod 4 = 0) And (rok Mod 100 <> 0)) Or ((rok Mod 4 = 0) And (rok Mod 100 = 0) And (rok Mod 400 = 0)) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Shared Sub show(rok)
+        Console.WriteLine("Czy rok jest przystępny: " + App.check(rok).ToString)
+    End Sub
+
+    Public Shared Sub enterkey()
+        Console.ReadKey()
+    End Sub
+
+End Class
---
+++ ISI_KM_PO_3_01/Program.vb
@@ -0,0 +1,11 @@
+﻿Module Program
+
+    Public Sub Main()
+        App.start()
+    End Sub
+
+    Friend Sub action(rok)
+        App.show(rok)
+        App.enterkey()
+    End Sub
+End Module
