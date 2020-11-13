62c98a8ddf0d0feeddaa556cdb0c59e0cd436d09
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-11-13 21:27:50 +0100

    3.11

---
+++ ISI_KM_PO_3_11/App.vb
@@ -0,0 +1,18 @@
+﻿Public Class App
+    Public Shared Function Licz(Liczba, Granica)
+        Dim Suma As Integer = 0 : Dim Wsk As Integer = 0
+        Do
+            Suma += Wsk + 1
+            Wsk += 1
+        Loop Until Suma > Granica
+        Return Wsk
+    End Function
+
+    Public Shared Sub Show(Liczba, Granica)
+        Console.WriteLine($"Ilosc liczb, których suma przekracza 100 to {Licz(Liczba, Granica)}")
+    End Sub
+
+    Public Shared Sub enterKey()
+        Console.ReadKey()
+    End Sub
+End Class
---
+++ ISI_KM_PO_3_11/Program.vb
@@ -0,0 +1,5 @@
+﻿Module Module1
+    Sub Main()
+        App.Show(1, 100) : App.enterKey()
+    End Sub
+End Module
