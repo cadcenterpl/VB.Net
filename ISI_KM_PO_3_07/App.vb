48348d80844e848bdfbc5d15b71eeedc43680b60
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-11-13 18:28:19 +0100

    3.07

---
+++ ISI_KM_PO_3_07/App.vb
@@ -0,0 +1,27 @@
+﻿Public Class App
+    Public Shared Sub Start()
+        Console.Write("Wpisz numer dnia tygodnia: ") : Dim Dzień As String = Console.ReadLine() : Program.Action(Dzień)
+    End Sub
+
+    Public Shared Function Check(Dzień)
+        Select Case Dzień
+            Case 1 : Return "Poniedziałek"
+            Case 2 : Return "Wtorek"
+            Case 3 : Return "Środa"
+            Case 4 : Return "Czwartek"
+            Case 5 : Return "Piątek"
+            Case 6 : Return "Sobota"
+            Case 7 : Return "Niedziela"
+            Case Else : Return "Błędny numer dnia tygodnia - (1-7)" : Program.Main()
+        End Select
+    End Function
+
+    Public Shared Sub Show(Dzień)
+        Console.WriteLine($"{Dzień}. dzień tygodnia to: {Check(Dzień)}")
+    End Sub
+
+    Public Shared Sub enterKey()
+        Console.ReadKey()
+    End Sub
+
+End Class
---
+++ ISI_KM_PO_3_07/Program.vb
@@ -0,0 +1,10 @@
+﻿Module Program
+
+    Sub Main()
+        App.Start() : End Sub
+
+    Public Sub Action(Dzień)
+        App.Show(Dzień) : App.enterKey()
+    End Sub
+
+End Module
