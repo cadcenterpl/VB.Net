3403dde1150d41addde3cd7b7a7543e212226bd4
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-11-13 21:07:38 +0100

    3.10

---
+++ ISI_KM_PO_3_10/App.vb
@@ -0,0 +1,21 @@
+﻿Public Class App
+    Public Shared Sub Start()
+        Console.Write("Podaj liczbę: ") : Dim Liczba As Integer = Console.ReadLine() : Program.Action(Liczba)
+    End Sub
+
+    Public Shared Function Licz(Liczba)
+        Dim Iloczyn As Integer = 1
+        For i As Integer = 0 To Liczba - 1
+            Iloczyn *= (i + 1)
+        Next
+        Return Iloczyn
+    End Function
+
+    Public Shared Sub Show(Liczba)
+        Console.WriteLine($"Silnia z {Liczba} wynosi: {Licz(Liczba)}" + vbCrLf) : Program.Main()
+    End Sub
+
+    Public Shared Sub enterKey()
+        Console.ReadKey()
+    End Sub
+End Class
---
+++ ISI_KM_PO_3_10/Program.vb
@@ -0,0 +1,10 @@
+﻿Module Program
+
+    Sub Main()
+        App.Start()
+    End Sub
+
+    Public Sub Action(Liczba)
+        App.Show(Liczba) : App.enterKey()
+    End Sub
+End Module
