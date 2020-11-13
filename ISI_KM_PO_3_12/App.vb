ed4c1650e5f9ee8509da76735ccaaec56bd3483c
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-11-13 21:52:56 +0100

    3.12

---
+++ ISI_KM_PO_3_12/App.vb
@@ -0,0 +1,20 @@
+﻿Public Class App
+    Public Shared Sub Start()
+        Dim Lista As New List(Of Integer)
+        Dim Liczba As Integer : Dim Suma As Integer = 0
+        Do
+            Console.Write("Podaj liczbę: ")
+            Liczba = Console.ReadLine()
+            Suma += Liczba
+        Loop Until Liczba = 0
+        Program.Action(Suma)
+    End Sub
+
+    Public Shared Sub Show(Suma)
+        Console.WriteLine($"Suma wybranych liczb przed wciśnięciem 0 wynosi {Suma}" + vbCrLf) : Program.Main()
+    End Sub
+
+    Public Shared Sub enterKey()
+        Console.ReadKey()
+    End Sub
+End Class
---
+++ ISI_KM_PO_3_12/Program.vb
@@ -0,0 +1,9 @@
+﻿Module Program
+    Sub Main()
+        App.Start()
+    End Sub
+
+    Public Sub Action(Suma)
+        App.Show(Suma) : App.enterKey()
+    End Sub
+End Module
