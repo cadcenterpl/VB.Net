b27a25bd696e4e8f5ef718ec5f56041e1f10e991
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-11-13 17:29:19 +0100

    3.05

---
+++ ISI_KM_PO_3_)5/App.vb
@@ -0,0 +1,22 @@
+﻿Public Class App
+    Public Shared Sub Start()
+        Console.WriteLine("Wprowadź współczynniki A,B C równania Ax^2+Bx+C=0")
+        Console.Write("Podaj A: ") : Dim A As Double = Console.ReadLine()
+        Console.Write("Podaj B: ") : Dim B As Double = Console.ReadLine()
+        Console.Write("Podaj C: ") : Dim C As Double = Console.ReadLine()
+        Program.Action(A, B, C)
+    End Sub
+    Public Shared Function Check(A, B, C)
+        Dim Delta As Double = B * B - (4 * A * C)
+        Select Case Delta
+            Case > 0 : Return 2 : Case = 0 : Return 1 : Case < 0 : Return 0
+        End Select
+
+    End Function
+    Public Shared Sub Show(A, B, C)
+        Console.WriteLine($"Ilość pierwiastków równania {A}x^2+{B}x+{C}=0: {Check(A, B, C)}")
+    End Sub
+    Public Shared Sub enterKey()
+        Console.ReadKey()
+    End Sub
+End Class
---
+++ ISI_KM_PO_3_)5/Program.vb
@@ -0,0 +1,8 @@
+﻿Module Program
+
+    Public Sub Main()
+        App.Start() : End Sub
+    Public Sub Action(A, B, C)
+        App.Show(A, B, C) : App.enterKey() : End Sub
+
+End Module
