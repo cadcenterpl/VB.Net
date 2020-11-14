9104c399a2833cc33244f12b484cb417523350ec
Kamil Martenczuk <155802@student.uwm.edu.pl>
2020-11-14 01:31:18 +0100

    3.15

---
+++ ISI_KM_PO_3_15/App.vb
@@ -0,0 +1,36 @@
+﻿Public Class App
+    Public Shared Function Licz(Monety, Indeks, Kwota, Wartość)
+
+        Static Wsk As Integer = 0
+        Dim Ilość As Integer = Kwota / Monety(Indeks)
+        If Indeks > 0 Then
+            For i As Integer = 0 To Ilość
+                If (i > 0) Then
+                    Console.Write(Wartość)
+                End If
+                Dim Obecne As String = ""
+                For j As Integer = 0 To i - 1
+                    Obecne += Convert.ToString(Monety(Indeks)) + " "
+                Next
+                Console.Write(Obecne)
+                Licz(Monety, Indeks - 1, Kwota - Monety(Indeks) * i, Wartość + Obecne)
+            Next
+        Else
+            For i As Integer = 0 To Ilość - 1
+                Console.Write(Monety(Indeks))
+            Next
+            Console.WriteLine()
+            Wsk += 1
+        End If
+        Return Wsk
+    End Function
+
+
+    Public Shared Sub Show(Monety, Kwota)
+        Console.WriteLine($"Mozliwe kombinacje to: " + vbCrLf) : Licz(Monety, Monety.Count - 1, Kwota, "")
+    End Sub
+
+    Public Shared Sub enterKey()
+        Console.ReadKey()
+    End Sub
+End Class
---
+++ ISI_KM_PO_3_15/Program.vb
@@ -0,0 +1,10 @@
+﻿Module Program
+    Sub Main()
+        Static A As Integer = 1
+        Static B As Integer = 2
+        Static C As Integer = 5
+        Static Kwota As Integer = 10
+        Dim Monety As New List(Of Integer) : Monety.Add(A) : Monety.Add(B) : Monety.Add(C)
+        App.Show(Monety, Kwota) : App.enterKey()
+    End Sub
+End Module
