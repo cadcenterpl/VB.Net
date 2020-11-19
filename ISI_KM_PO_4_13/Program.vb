e0dea0435134a776558fe3fb586f2163422dadb2
Kamil Martenczuk <kami.martenczuk@outlook.com>
2020-11-19 16:56:19 +0100

    4.13a

--- ISI_KM_PO_4_13/Program.vb
+++ ISI_KM_PO_4_13/Program.vb
@@ -2,7 +2,7 @@ Imports System
 
 Module Program
     Sub Main()
-        Dim Aplikacja As App = New App("Zliczanie wierszy i znakow")
+        Dim Aplikacja As App = New App("Zliczanie powielonych wyrazow")
         Dim LancuchUzytkownika As Lancuch = New Lancuch(Aplikacja.OdpUzytkownika("Wpisz zdanie: ", "*"))
         Aplikacja.Wydrukuj(LancuchUzytkownika.Tresc) : Aplikacja.Wyswietl("")
         Aplikacja.Wydrukuj($"Lista powielonych slow: {LancuchUzytkownika.LiczPowieloneSlowa}") 
