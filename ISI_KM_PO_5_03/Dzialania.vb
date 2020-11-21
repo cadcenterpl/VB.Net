41bf249b41147af1d07670f2c6666bd85f469114
Kamil Martenczuk <kami.martenczuk@outlook.com>
2020-11-21 20:11:25 +0100

    5.03

--- ISI_KM_PO_5_03/Dzialania.vb
+++ ISI_KM_PO_5_03/Dzialania.vb
@@ -1,5 +1,5 @@
 Public Class Dzialania
-    Public Shared Function Przesun(ByRef punkt() As Double, ByVal wek() AS Double)
+    Public Shared Sub Przesun(ByRef punkt() As Double, ByVal wek() AS Double)
         punkt(0)+=wek(0) : punkt(1)+=wek(1)
-    End Function
+    End Sub
 End Class
